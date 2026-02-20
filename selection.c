753739df721989aa1bbce5dc2fde732bff7e52e7
Abdul Wahid Mulla <pvdkg2hfwx@privaterelay.appleid.com>
2026-02-20 12:11:20 +0530

    commit

---
+++ selection.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int n, i, j, min_index, temp;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Selection Sort
+    for(i = 0; i < n - 1; i++) {
+        min_index = i;
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min_index])
+                min_index = j;
+        }
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+    printf("Sorted array:\n");
+    for(i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    return 0;
+}
